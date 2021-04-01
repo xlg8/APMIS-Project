66244742c6365ab786e67d25f2174e981361d716
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2021-04-01 09:26:20 +0200

    [#4959] remove openAPI generation for Required annotation

--- sormas-rest/src/main/java/de/symeda/sormas/rest/swagger/AttributeConverter.java
+++ sormas-rest/src/main/java/de/symeda/sormas/rest/swagger/AttributeConverter.java
@@ -15,84 +15,67 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class AttributeConverter extends ModelResolver {
-    public static final String XPROP_PREFIX = "x-sormas-";
-    public static final String XPROP_PERSONAL_DATA = XPROP_PREFIX + "personal-data";
-    public static final String XPROP_FOR_COUNTRIES = XPROP_PREFIX + "countries-for";
-    public static final String XPROP_EXCEPT_COUNTRIES = XPROP_PREFIX + "countries-except";
-    public static final String XPROP_DEPENDS_ON = XPROP_PREFIX + "depends-on";
-    public static final String XPROP_DISEASES = XPROP_PREFIX + "diseases";
-    public static final String XPROP_OUTBREAKS = XPROP_PREFIX + "outbreaks";
-    public static final String XPROP_COMPLICATIONS = XPROP_PREFIX + "complications";
 
-    public AttributeConverter(ObjectMapper mapper) {
-        super(mapper);
-    }
+	public static final String XPROP_PREFIX = "x-sormas-";
+	public static final String XPROP_PERSONAL_DATA = XPROP_PREFIX + "personal-data";
+	public static final String XPROP_FOR_COUNTRIES = XPROP_PREFIX + "countries-for";
+	public static final String XPROP_EXCEPT_COUNTRIES = XPROP_PREFIX + "countries-except";
+	public static final String XPROP_DEPENDS_ON = XPROP_PREFIX + "depends-on";
+	public static final String XPROP_DISEASES = XPROP_PREFIX + "diseases";
+	public static final String XPROP_OUTBREAKS = XPROP_PREFIX + "outbreaks";
+	public static final String XPROP_COMPLICATIONS = XPROP_PREFIX + "complications";
 
-    @Override
-    protected void applyBeanValidatorAnnotations(Schema property, Annotation[] annotations, Schema parent) {
-        super.applyBeanValidatorAnnotations(property, annotations, parent);
-        Map<String, Annotation> annos = new HashMap<String, Annotation>();
-        if (annotations != null) {
-            for (Annotation anno : annotations) {
-                annos.put(anno.annotationType().getName(), anno);
-            }
-        }
-        if (parent != null && annos.containsKey("de.symeda.sormas.api.utils.Required")) {
-            addRequiredItem(parent, property.getName());
-        }
-    }
+	public AttributeConverter(ObjectMapper mapper) {
+		super(mapper);
+	}
 
+	@Override
+	public Schema<?> resolve(AnnotatedType annotatedType, ModelConverterContext modelConverterContext, Iterator<ModelConverter> iterator) {
+		Schema schema = super.resolve(annotatedType, modelConverterContext, iterator);
 
-    @Override
-    public Schema<?> resolve(AnnotatedType annotatedType, ModelConverterContext modelConverterContext, Iterator<ModelConverter> iterator) {
-        Schema schema = super.resolve(annotatedType, modelConverterContext, iterator);
+		if (schema != null && annotatedType.getCtxAnnotations() != null && annotatedType.isSchemaProperty()) {
 
-        //@formatter:off
-        if (schema != null && annotatedType.getCtxAnnotations() != null
-                && annotatedType.isSchemaProperty()) {
+			for (Annotation a : annotatedType.getCtxAnnotations()) {
+				if (a.annotationType() == PersonalData.class) {
+					// Outbreak visibility documentation
+					schema.addExtension(XPROP_PERSONAL_DATA, true);
 
-            for (Annotation a : annotatedType.getCtxAnnotations()) {
-                if (a.annotationType() == PersonalData.class) {
-                    // Outbreak visibility documentation
-                    schema.addExtension(XPROP_PERSONAL_DATA, true);
+				} else if (a.annotationType() == Outbreaks.class) {
+					// Complications documentation
+					schema.addExtension(XPROP_OUTBREAKS, true);
 
-                } else if (a.annotationType() == Outbreaks.class) {
-                    // Complications documentation
-                    schema.addExtension(XPROP_OUTBREAKS, true);
+				} else if (a.annotationType() == Complication.class) {
+					// Complications documentation
+					schema.addExtension(XPROP_COMPLICATIONS, true);
 
-                } else if (a.annotationType() == Complication.class) {
-                    // Complications documentation
-                    schema.addExtension(XPROP_COMPLICATIONS, true);
+				} else if (a.annotationType() == Diseases.class) {
+					// Disease association documentation
+					Disease[] diseases = ((Diseases) a).value();
+					if (diseases.length > 0) {
+						schema.addExtension(XPROP_DISEASES, diseases);
+					}
 
-                } else if (a.annotationType() == Diseases.class) {
-                    // Disease association documentation
-                    Disease[] diseases = ((Diseases) a).value();
-                    if (diseases.length > 0) {
-                        schema.addExtension(XPROP_DISEASES, diseases);
-                    }
+				} else if (a.annotationType() == DependantOn.class) {
+					// Field dependency documentation
+					schema.addExtension(XPROP_DEPENDS_ON, ((DependantOn) a).value());
 
-                } else if (a.annotationType() == DependantOn.class) {
-                    // Field dependency documentation
-                    schema.addExtension(XPROP_DEPENDS_ON, ((DependantOn) a).value());
+				} else if (a.annotationType() == HideForCountries.class) {
+					// Documentation of countries to hide the field from
+					if (schema.getExtensions() != null) {
+						schema.getExtensions().remove(XPROP_FOR_COUNTRIES);
+					}
+					schema.addExtension(XPROP_EXCEPT_COUNTRIES, ((HideForCountries) a).countries());
 
-                } else if (a.annotationType() == HideForCountries.class) {
-                    // Documentation of countries to hide the field from
-                    if (schema.getExtensions() != null) {
-                        schema.getExtensions().remove(XPROP_FOR_COUNTRIES);
-                    }
-                    schema.addExtension(XPROP_EXCEPT_COUNTRIES, ((HideForCountries) a).countries());
+				} else if (a.annotationType() == HideForCountriesExcept.class) {
+					// Documentation of countries to show field for
+					if (schema.getExtensions() == null || !schema.getExtensions().containsKey(XPROP_EXCEPT_COUNTRIES)) {
+						schema.addExtension(XPROP_FOR_COUNTRIES, ((HideForCountriesExcept) a).countries());
+					}
+				}
+			}
+		}
 
-                } else if (a.annotationType() == HideForCountriesExcept.class) {
-                    // Documentation of countries to show field for
-                    if (schema.getExtensions() == null || !schema.getExtensions().containsKey(XPROP_EXCEPT_COUNTRIES)) {
-                        schema.addExtension(XPROP_FOR_COUNTRIES, ((HideForCountriesExcept) a).countries());
-                    }
-                }
-            }
-            //@formatter:on
-        }
+		return schema;
 
-        return schema;
-
-    }
+	}
 }
