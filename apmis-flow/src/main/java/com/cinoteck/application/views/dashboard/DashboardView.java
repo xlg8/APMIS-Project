c4b3793a870dfe2ea65f2650c24432212e8639b9
sundayAdu <91499626+sundayAdu@users.noreply.github.com>
2023-06-16 14:55:11 +0100

    -Refactoring dashboard view #202

--- apmis-flow/src/main/java/com/cinoteck/application/views/dashboard/DashboardView.java
+++ apmis-flow/src/main/java/com/cinoteck/application/views/dashboard/DashboardView.java
@@ -6,35 +6,22 @@ import static de.symeda.sormas.api.campaign.CampaignJurisdictionLevel.DISTRICT;
 import static de.symeda.sormas.api.campaign.CampaignJurisdictionLevel.REGION;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.UUID;
 
 import org.apache.commons.text.WordUtils;
-
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.JavaScript;
-import com.vaadin.flow.component.dependency.StyleSheet;
-import com.vaadin.flow.component.dnd.DragSource;
 import com.cinoteck.application.views.MainLayout;
 import com.vaadin.flow.component.Component;
-<<<<<<< HEAD
-import com.vaadin.flow.component.Direction;
-import com.vaadin.flow.component.UI;
-=======
-import com.vaadin.flow.component.Text;
->>>>>>> branch 'development' of https://github.com/xlg8/APMIS-Project.git
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.tabs.Tab;
-import com.vaadin.flow.component.tabs.TabSheet;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
@@ -42,6 +29,7 @@ import com.vaadin.flow.function.SerializableSupplier;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLayout;
+import com.vaadin.flow.server.VaadinSession;
 
 import de.symeda.sormas.api.FacadeProvider;
 import de.symeda.sormas.api.campaign.CampaignDto;
@@ -58,6 +46,7 @@ import de.symeda.sormas.api.user.UserDto;
 @Route(value = "dashboard", layout = MainLayout.class)
 
 @JavaScript("https://code.highcharts.com/highcharts.js")
+@JavaScript("https://code.highcharts.com/modules/variable-pie.js")
 @JavaScript("https://code.highcharts.com/modules/exporting.js")
 @JavaScript("https://code.highcharts.com/modules/export-data.js")
 @JavaScript("https://code.highcharts.com/modules/accessibility.js")
@@ -67,6 +56,11 @@ import de.symeda.sormas.api.user.UserDto;
 //@JavaScript("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js")
 public class DashboardView extends VerticalLayout implements RouterLayout {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1851726752523985165L;
+
 	protected CampaignDashboardDataProvider dataProvider;
 
 	Binder<UserDto> binder = new BeanValidationBinder<>(UserDto.class);
@@ -74,7 +68,7 @@ public class DashboardView extends VerticalLayout implements RouterLayout {
 
 	CampaignSummaryGridView campaignSummaryGridView = new CampaignSummaryGridView();
 
-	private Map<TabSheet, Component> tabComponentMap = new LinkedHashMap<>();
+	// private Map<TabSheet, Component> tabComponentMap = new LinkedHashMap<>();
 	ComboBox<CampaignReferenceDto> campaign = new ComboBox<>();
 	ComboBox<CampaignPhase> campaignPhase = new ComboBox<>();
 	ComboBox<AreaReferenceDto> region = new ComboBox<>();
@@ -82,26 +76,25 @@ public class DashboardView extends VerticalLayout implements RouterLayout {
 	ComboBox<DistrictReferenceDto> district = new ComboBox<>();
 	ComboBox<CommunityReferenceDto> cluster = new ComboBox<>();
 	Select<CampaignJurisdictionLevel> groupby = new Select<>();
-	
-	
+
 	List<CampaignReferenceDto> campaigns;
 	List<CampaignReferenceDto> campaignPhases;
 	List<AreaReferenceDto> regions;
 	List<RegionReferenceDto> provinces;
 	List<DistrictReferenceDto> districts;
 	List<CommunityReferenceDto> communities;
-	
+
 	private boolean isSubAvaialable = false;
 	private CampaignJurisdictionLevel campaignJurisdictionLevel;
 
+	private String listerCheck = "";
+	
+	private Div mainContentContainerx;
+
 	public DashboardView() {
-<<<<<<< HEAD
-		setSpacing(true);
+		setSpacing(false);
 		
 	//	UI.getCurrent().setDirection(Direction.RIGHT_TO_LEFT);
-=======
-		setSpacing(false);
->>>>>>> branch 'development' of https://github.com/xlg8/APMIS-Project.git
 
 		dataProvider = new CampaignDashboardDataProvider();
 
@@ -111,65 +104,58 @@ public class DashboardView extends VerticalLayout implements RouterLayout {
 
 		campaign.getStyle().set("padding-top", "0px");
 		campaign.setClassName("col-sm-6, col-xs-6");
-
-		campaign.addValueChangeListener(e -> {
-			dataProvider.setCampaign((CampaignReferenceDto) campaign.getValue());
-			Notification.show("Cmapaign has chnaged TODO = " + e.getValue().getCaption());
-			// dashboardView.refreshDashboard();
-		});
-
-		final CampaignReferenceDto lastStartedCampaign = dataProvider.getLastStartedCampaign();
-
+		
+		
 		campaignPhase.setLabel("Campaign Phase");
 //		campaignPhases = FacadeProvider.getCampaignFacade().getAllActiveCampaignsAsReference()
 		campaignPhase.setItems(CampaignPhase.values());
 		campaignPhase.getStyle().set("padding-top", "0px");
 		campaignPhase.setClassName("col-sm-6, col-xs-6");
+		
 
 		// campaignFormPhaseSelector = new
 		// CampaignFormPhaseSelector(lastStartedCampaign);
 
-		campaignPhase.addValueChangeListener(e -> {
-			dataProvider.setFormType(campaignPhase.getValue().toString().toLowerCase());
-			// dataProvider.refreshDashboard();
-		});
+		System.out.println(VaadinSession.getCurrent().getSession().getAttribute("campaignPhase"));
+		System.out.println(VaadinSession.getCurrent().getSession().getAttribute("campaign"));
+		
+		if(VaadinSession.getCurrent().getSession().getAttribute("campaignPhase") != null) {
+				String sessionCheckPhase = VaadinSession.getCurrent().getSession().getAttribute("campaignPhase").toString();
+				String sessionCheckCampaign = VaadinSession.getCurrent().getSession().getAttribute("campaign").toString();
+		
+				
+				CampaignReferenceDto sx = FacadeProvider.getCampaignFacade().getReferenceByUuid(sessionCheckCampaign);
+				dataProvider.setCampaign(sx);
+				campaign.setValue(sx);
+				campaignPhase.setValue(CampaignPhase.valueOf(sessionCheckPhase.replace("-campaign", "").toUpperCase()));
+				dataProvider.setFormType(sessionCheckPhase);
+				
+					
+		}else {
+			final CampaignReferenceDto lastStartedCampaign = dataProvider.getLastStartedCampaign();
 
-		if (lastStartedCampaign != null) {
-			campaign.setValue(lastStartedCampaign);
-			campaignPhase.setValue(CampaignPhase.INTRA);
+			if (lastStartedCampaign != null) {
+				campaign.setValue(lastStartedCampaign);
+				campaignPhase.setValue(CampaignPhase.INTRA);
+			}
+
+			dataProvider.setCampaign(lastStartedCampaign);
+			dataProvider.setFormType(CampaignPhase.INTRA.toString().toLowerCase());
 		}
-
-		Notification.show(CampaignPhase.INTRA.toString().toLowerCase());
-
-		dataProvider.setCampaign(lastStartedCampaign);
-		dataProvider.setFormType(CampaignPhase.INTRA.toString().toLowerCase());
-
+		
+		
+		//Filter initializers
 		region.setLabel("Region");
 		binder.forField(region).bind(UserDto::getArea, UserDto::setArea);
 		regions = FacadeProvider.getAreaFacade().getAllActiveAsReference();
+		region.setClearButtonVisible(true);
 		region.setItems(regions);
-		region.setValue(FacadeProvider.getAreaFacade().getAreaReferenceByUuid("W5R34K-APYPCA-4GZXDO-IVJWKGIM"));
-		region.addValueChangeListener(e -> {
-			Notification.show("area changing... " + e.getValue());
-			changeCampaignJuridictionLevel(campaignJurisdictionLevel.AREA);
-			dataProvider.setArea(e.getValue());
-			provinces = FacadeProvider.getRegionFacade().getAllActiveByArea(e.getValue().getUuid());
-			province.setItems(provinces);
-		});
 		
-		
-		
-		
-		
-		//TODO: this is only for debugging purpose, please remove after test
+
+		// TODO: this is only for debugging purpose, please remove after test
 		dataProvider.setCampaignJurisdictionLevelGroupBy(campaignJurisdictionLevel.AREA);
-		dataProvider.setArea(FacadeProvider.getAreaFacade().getAreaReferenceByUuid("W5R34K-APYPCA-4GZXDO-IVJWKGIM"));
-		
-		
-		
-		
-		
-		
+		//dataProvider.setArea(FacadeProvider.getAreaFacade().getAreaReferenceByUuid("W5R34K-APYPCA-4GZXDO-IVJWKGIM"));
+
 		region.getStyle().set("padding-top", "0px");
 		region.setClassName("col-sm-6, col-xs-6");
 
@@ -177,56 +163,29 @@ public class DashboardView extends VerticalLayout implements RouterLayout {
 		binder.forField(province).bind(UserDto::getRegion, UserDto::setRegion);
 		provinces = FacadeProvider.getRegionFacade().getAllActiveAsReference();
 		province.setItems(provinces);
-		province.addValueChangeListener(e -> {
-			Notification.show("Region changing... " + e.getValue());
-			changeCampaignJuridictionLevel(campaignJurisdictionLevel.REGION);
-			dataProvider.setRegion(e.getValue());
-			districts = FacadeProvider.getDistrictFacade().getAllActiveByRegion(e.getValue().getUuid());
-			district.setItems(districts);
-		});
 		province.getStyle().set("padding-top", "0px");
 		province.setClassName("col-sm-6, col-xs-6");
-
 	//	province.setEnabled(false);
 
 		district.setLabel("District");
 		binder.forField(district).bind(UserDto::getDistrict, UserDto::setDistrict);
 		districts = FacadeProvider.getDistrictFacade().getAllActiveAsReference();
 		district.setItems(districts);
-		district.addValueChangeListener(e -> {
-			changeCampaignJuridictionLevel(campaignJurisdictionLevel.DISTRICT);
-			dataProvider.setDistrict(e.getValue());
-			communities = FacadeProvider.getCommunityFacade().getAllActiveByDistrict(e.getValue().getUuid());
-			cluster.setItemLabelGenerator(CommunityReferenceDto::getCaption);
-			cluster.setItems(communities);
-		});
 		district.getStyle().set("padding-top", "0px");
 		district.setClassName("col-sm-6, col-xs-6");
-
 	//	district.setEnabled(false);
 
-		cluster.setLabel("Cluster");
-
-		cluster.getStyle().set("padding-top", "0px");
-		cluster.setClassName("col-sm-6, col-xs-6");
-
-		
-		
-		
-		
 		groupby.setLabel("Group By");
 		groupby.setItems(campaignJurisdictionLevel.values());
 		groupby.getStyle().set("padding-top", "0px");
 		groupby.setClassName("col-sm-6, col-xs-6");
 		
-		groupby.addValueChangeListener(e -> {
-			dataProvider.setCampaignJurisdictionLevelGroupBy(e.getValue());
-		});
-		
-		HorizontalLayout vlayout = new HorizontalLayout();
+		//TODO should check user assignment first and set respectively
+		groupby.setValue(campaignJurisdictionLevel.AREA);
+
 		
 		HorizontalLayout selectFilterLayout = new HorizontalLayout(campaign, campaignPhase, region, province, district,
-				cluster, groupby);
+				/*cluster,*/ groupby);
 		selectFilterLayout.setClassName("row pl-3");
 		VerticalLayout selectFilterLayoutparent = new VerticalLayout(selectFilterLayout);
 		selectFilterLayoutparent.getStyle().set("padding", "0px");
@@ -238,7 +197,9 @@ public class DashboardView extends VerticalLayout implements RouterLayout {
 		displayFilters.getStyle().set("margin-top", "12px");
 		displayFilters.setIcon(new Icon(VaadinIcon.SLIDERS));
 
-
+		
+		
+		
 		
 		//filter listeners
 		campaign.addValueChangeListener(e -> {
@@ -329,12 +290,13 @@ public class DashboardView extends VerticalLayout implements RouterLayout {
 				province.setEnabled(false);
 				district.setEnabled(false);
 
-
 			} else {
 				selectFilterLayoutparent.setVisible(false);
 				displayFilters.setText("Show Filters");
 			}
 		});
+
+
 
 		UUID uuid = UUID.randomUUID();
 		
@@ -354,101 +316,129 @@ public class DashboardView extends VerticalLayout implements RouterLayout {
 		
 		mainContentContainer.removeAll();
 		mainContentContainer.setWidthFull();
-
+		
 		Tabs mtabs = new Tabs();
+		mtabs.removeAll();
+						
 		mtabs.setId("maintab");
 		mtabs.getStyle().set("background", "#434343");
 		mtabs.getStyle().set("color", "#ffffff00");
 		mtabs.setWidthFull();
+
 		Tabs sTabs = new Tabs();
 		sTabs.setId("subtabs");
+		sTabs.getStyle().set("background", "#0D6938");
 		
-		sTabs.setWidthFull();
-		Div contentContainer = new Div();
-
 		// contentContainer.getStyle().set("background", "#f1f4f6");
-		final List<String> mainTabs = new ArrayList<>(dataProvider.getTabIds());
-		int ctr = 0;
-		
-		// creating bucket tabs to hold tabs and subtabs
-		for (String tabIdc : mainTabs) {
-			ctr++;
+				final List<String> mainTabs = new ArrayList<>(dataProvider.getTabIds());
+				int ctr = 0;
+				int defctr = 0;
 
-			String mntabId = WordUtils.capitalizeFully(tabIdc);
-			Tab tabx = new Tab(mntabId);
-			tabx.setId("main_" + mntabId);
-			tabx.getStyle().set("font-weight", "600");
-			mtabs.add(tabx);
+				String firstMntabId = null;
+				String firstSubtabId = null;
 
-			// this has to be moved to listener
-			if (ctr == 1) {
-				final List<String> subTabx = new ArrayList<>(dataProvider.getSubTabIds(mntabId));
-				sTabs.setVisible(subTabx.size() > 1);
-				isSubAvaialable = subTabx.size() > 1 ;
-				for (String sbTabId : subTabx) {
-//					String sbtabId = WordUtils.capitalizeFully(tabIdc);
-					Tab stabx = new Tab(sbTabId);
-					stabx.setId("submain_" + sbTabId);
-					sTabs.add(stabx);
-					sTabs.getStyle().set("margin-bottom", "1em");
-					stabx.getStyle().set("color", "#0D6938");
-					stabx.getStyle().set("font-weight", "500");
-					sTabs.getStyle().set("background", "white");
-					
+				// creating bucket tabs to hold tabs and subtabs
+				for (String tabIdc : mainTabs) {
+					ctr++;
 
-				}
-			}
-		}
+					String mntabId = WordUtils.capitalizeFully(tabIdc);
+					Tab tabx = new Tab(mntabId);
+					tabx.setId("main_" + mntabId);
+					mtabs.add(tabx);
 
-		mtabs.addSelectedChangeListener(e -> {
-			int listnrCtr = 0;
-			
-				final List<String> subTabx = new ArrayList<>(dataProvider.getSubTabIds(e.getSelectedTab().getId().get().toString().replaceAll("main_", "")));
-				isSubAvaialable = subTabx.size() > 1 ;
-				sTabs.setVisible(isSubAvaialable);
-				sTabs.removeAll();
-				String firstSbTabId = "";
-					
+					// this has to be moved to listener
+					if (ctr == 1) {
+
+						final List<String> subTabx = new ArrayList<>(dataProvider.getSubTabIds(mntabId));
+						sTabs.setVisible(subTabx.size() > 1);
+						isSubAvaialable = subTabx.size() > 1;
 						for (String sbTabId : subTabx) {
-							listnrCtr++;
-							if(listnrCtr == 1) {
-								firstSbTabId = sbTabId;
-							}
-		//					String sbtabId = WordUtils.capitalizeFully(tabIdc);
+							defctr++;
+
+//							String sbtabId = WordUtils.capitalizeFully(tabIdc);
 							Tab stabx = new Tab(sbTabId);
 							stabx.setId("submain_" + sbTabId);
-							stabx.getStyle().set("color", "#0D6938");
-							stabx.getStyle().set("font-weight", "600");
-							sTabs.getStyle().set("margin-bottom", "1em");
-							sTabs.getStyle().set("background", "white");
 							sTabs.add(stabx);
-		
+							
+							//havesting first tab and sub tab ids
+							if (defctr == 1) {
+							//	getElement().executeJs("alert($1);", mntabId);
+								
+								firstMntabId = mntabId;
+								firstSubtabId = sbTabId;
+							}
 						}
-//						Notification.show(e.getSelectedTab().getId().get().toString().replaceAll("main_", ""));
-//						Notification.show(firstSbTabId);
 						
-			contentContainer.removeAll();
-			
-			
-			
-			contentContainer.add(campaignSummaryGridView.CampaignSummaryGridViewInit(
-					e.getSelectedTab().getId().get().replaceAll("main_", ""), dataProvider,
-					campaignPhase.getValue(), firstSbTabId));
-			
-			
-			
-		
-		});
+					}
+				}
 
-		contentContainer.setWidthFull();
-		contentContainer.setId("tabsSheet");
-		contentContainer.setSizeFull();
-		
-		vlayout.setAlignItems(Alignment.END);
-		vlayout.add(displayFilters,selectFilterLayoutparent);
+				mtabs.addSelectedChangeListener(e -> {
+					int listnrCtr = 0;
 
-		add(vlayout, mtabs, sTabs, contentContainer);
-		setSizeFull();
+					final List<String> subTabx = new ArrayList<>(
+							dataProvider.getSubTabIds(e.getSelectedTab().getId().get().toString().replaceAll("main_", "")));
+					isSubAvaialable = subTabx.size() > 1;
+					sTabs.setVisible(isSubAvaialable);
+					sTabs.removeAll();
+					String firstSbTabId = "";
+
+					for (String sbTabId : subTabx) {
+						listnrCtr++;
+						if (listnrCtr == 1) {
+							firstSbTabId = sbTabId;
+						}
+						// String sbtabId = WordUtils.capitalizeFully(tabIdc);
+						Tab stabx = new Tab(sbTabId);
+						stabx.setId("submain_" + sbTabId);
+						sTabs.add(stabx);
+
+					}
+//								//Notification.show(e.getSelectedTab().getId().get().toString().replaceAll("main_", ""));
+//								//Notification.show(firstSbTabId);
+
+					contentContainer.removeAll();
+
+					listerCheck = e.getSelectedTab().getId().get().replaceAll("main_", "");
+
+					contentContainer.add(campaignSummaryGridView.CampaignSummaryGridViewInit(
+							e.getSelectedTab().getId().get().replaceAll("main_", ""), dataProvider, campaignPhase.getValue(),
+							firstSbTabId));
+
+				});
+
+				sTabs.addSelectedChangeListener(e -> {
+					String listnrCtr = listerCheck;
+					if (e.getSelectedTab() != null) {
+						//System.out.print(isSubAvaialable);
+						String listnrCtrx = e.getSelectedTab().getId().get().replaceAll("submain_", "");
+
+						// Notification.show(listnrCtr +" _____________________________ "+listnrCtrx);
+
+						contentContainer.removeAll();
+						contentContainer.add(campaignSummaryGridView.CampaignSummaryGridViewInit(listnrCtr, dataProvider,
+								campaignPhase.getValue(), listnrCtrx));
+
+					}
+				});
+				
+				
+				
+				if(firstMntabId != null && firstSubtabId != null) {
+				//	Notification.show(firstMntabId +" _+_+_+_+_+_+ "+ firstSubtabId+" ____________ "+campaignPhase.getValue());
+					listerCheck = firstMntabId;
+					contentContainer.removeAll();
+					contentContainer.add(campaignSummaryGridView.CampaignSummaryGridViewInit(firstMntabId, dataProvider,
+							campaignPhase.getValue(), firstSubtabId));
+				}
+				contentContainer.getStyle().set("margin-top", "0.4rem");
+				
+				contentContainer.setId("tabsSheet");
+				contentContainer.setSizeFull();
+				
+				mainContentContainer.add(mtabs, sTabs, contentContainer);
+				return mainContentContainer;
+
+		
 	}
 
 	//
@@ -468,6 +458,11 @@ public class DashboardView extends VerticalLayout implements RouterLayout {
 //	
 
 	public class LazyComponent extends Div {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -3141958332173506396L;
+
 		public LazyComponent(SerializableSupplier<? extends Component> supplier) {
 			addAttachListener(e -> {
 				if (getElement().getChildCount() == 0) {
@@ -476,8 +471,7 @@ public class DashboardView extends VerticalLayout implements RouterLayout {
 			});
 		}
 	}
-	
-	
+
 	private void changeCampaignJuridictionLevel(CampaignJurisdictionLevel campaignJurisdictionLevelz) {
 		groupby.clear();
 		switch (campaignJurisdictionLevelz) {
@@ -495,7 +489,6 @@ public class DashboardView extends VerticalLayout implements RouterLayout {
 			break;
 		}
 
-		
 	}
 
 }
