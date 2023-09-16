fbe63cefa02484ebf62cf8a7c1f74103d0359a44
Yahaya Yusuf <yyahaya222@yahoo.com>
2023-09-16 21:38:12 +0100

    Form builder UI #202

--- apmis-flow/src/main/java/com/cinoteck/application/views/uiformbuilder/FormBuilderView.java
+++ apmis-flow/src/main/java/com/cinoteck/application/views/uiformbuilder/FormBuilderView.java
@@ -1,14 +1,23 @@
 package com.cinoteck.application.views.uiformbuilder;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+
+import org.docx4j.model.datastorage.XPathEnhancerParser.main_return;
 
 import com.cinoteck.application.views.MainLayout;
 import com.google.gson.Gson;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.formlayout.FormLayout.ResponsiveStep;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.Notification.Position;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
@@ -20,55 +29,80 @@ import de.symeda.sormas.api.campaign.form.CampaignFormElementType;
 @Route(value = "UI-Builder", layout = MainLayout.class)
 public class FormBuilderView extends FormLayout {
 
+	/**
+		 * 
+		 */
+	private static final long serialVersionUID = 1L;
+
 	private final Gson gson = new Gson();
-	private final Map<String, String> jsonData = new HashMap<>();
+	private final Map<String, String> jsonData = new LinkedHashMap<String, String>();
 
 	ComboBox<CampaignFormElementType> formType = new ComboBox<CampaignFormElementType>("Type");
-
-	ComboBox<String> formId = new ComboBox<>("Id");
+	TextField formId = new TextField("Id");
 	TextField caption = new TextField("Label");
 	Checkbox important = new Checkbox("Important");
 	TextField optionsKey = new TextField("Option Keys");
 	TextField optionsValue = new TextField("Option Values");
-	ComboBox<String> expression = new ComboBox<>("Expression");
-	ComboBox<String> dependingOn = new ComboBox<>("Depending On");
+	TextField expression = new TextField("Expression");
+	TextField dependingOn = new TextField("Depending On");
 	ComboBox<String> dependingOnValues = new ComboBox<>("Depending On Values");
-	ComboBox<String> styles = new ComboBox<>("Styles");
+	MultiSelectComboBox<String> styles = new MultiSelectComboBox<>("Styles");
+
 	ComboBox<String> constraints = new ComboBox<>("Constraints");
+	NumberField min = new NumberField("Min");
+	NumberField max = new NumberField("Max");
+
 	TextField errorMessage = new TextField("ErrorMessage");
 
 	Button generateJson = new Button("Generate Json");
 
+	Button clearTextArea = new Button("Clear Generated Json");
+
 	TextArea jsonContainer = new TextArea("Generated Json");
+
+	StringBuilder appender;
 
 	public FormBuilderView() {
 
-		this.setResponsiveSteps(new ResponsiveStep("0", 1), new ResponsiveStep("500px", 2));
+		this.addClassName("uibuilderview");
 
+		this.setResponsiveSteps(new ResponsiveStep("0", 1), new ResponsiveStep("500px", 2));
 		this.setColspan(jsonContainer, 2);
 		this.setColspan(generateJson, 2);
+		this.setColspan(clearTextArea, 2);
+
+		expression.setVisible(false);
+		dependingOn.setVisible(false);
+		dependingOnValues.setVisible(false);
+		min.setVisible(false);
+		max.setVisible(false);
 
 		configure();
 		valueChange();
 		add(formType, formId, caption, important, optionsKey, optionsValue, expression, dependingOn, dependingOnValues,
-				styles, constraints, errorMessage, generateJson, jsonContainer);		
+				styles, constraints, min, max, errorMessage, generateJson, clearTextArea, jsonContainer);
 	}
 
 	public void configure() {
 
+		optionsKey.setHelperText("Enter options keys in a comma seperated format like so Urban, Rural");
+		optionsValue.setHelperText("Enter options values in a comma seperated format like so Urban, Rural");
+		expression.setHelperText("Please enter expression value with care");
+		formId.setHelperText("Append \"-readonly\" at the end of the Id value. If you want it to be Read Only");
 		dependingOnValues.setItems("Yes", "No");
 		formType.setItems(CampaignFormElementType.values());
 		constraints.setItems("Expression", "Range");
-		styles.setItems("inline", "row", "first", "col-1", "col-2", "col-3", 
-				"col-4", "col-5", "col-6", "col-7", "col-8", "col-9", "col-10", "col-11", "col-12");
+		styles.setItems("inline", "row", "first", "col-1", "col-2", "col-3", "col-4", "col-5", "col-6", "col-7",
+				"col-8", "col-9", "col-10", "col-11", "col-12");
 	}
 
 	public void valueChange() {
-		
+
 		formType.addValueChangeListener(e -> {
-			
-			if (formType.getValue().toString().toLowerCase().equals("section")) {
-				
+
+			if (e.getValue().toString().toLowerCase().equals("section")
+					|| e.getValue().toString().toLowerCase().equals("daywise")) {
+
 				caption.setVisible(false);
 				important.setVisible(false);
 				optionsKey.setVisible(false);
@@ -79,20 +113,147 @@ public class FormBuilderView extends FormLayout {
 				styles.setVisible(false);
 				constraints.setVisible(false);
 				errorMessage.setVisible(false);
+			} else if (e.getValue().toString().toLowerCase().equals("number")
+					|| e.getValue().toString().toLowerCase().equals("range")) {
+
+				expression.setVisible(true);
+				dependingOn.setVisible(true);
+				dependingOnValues.setVisible(true);
 			} else {
-				
+
 				caption.setVisible(true);
 				important.setVisible(true);
 				optionsKey.setVisible(true);
 				optionsValue.setVisible(true);
-				expression.setVisible(true);
-				dependingOn.setVisible(true);
-				dependingOnValues.setVisible(true);
 				styles.setVisible(true);
 				constraints.setVisible(true);
 				errorMessage.setVisible(true);
 			}
+
+			jsonData.put("type", e.getValue().toString());
 		});
+
+		formId.addValueChangeListener(e -> {
+
+			jsonData.put("id", e.getValue().toString());
+		});
+
+		caption.addValueChangeListener(e -> {
+
+			jsonData.put("caption", e.getValue().toString());
+		});
+
+		important.addClickListener(e -> {
+
+			jsonData.put("important", "true");
+		});
+
+		optionsKey.addValueChangeListener(e -> {
+
+		});
+		
+//		 "options":[{ "key":"yes", "caption":"Yes" },{ "key":"no", "caption":"No"}]
+		optionsValue.addValueChangeListener(e -> {
+
+			
+		});
+
+		dependingOn.addValueChangeListener(e -> {
+
+			if (e.getValue() != null) {
+
+				jsonData.put("dependingOn", e.getValue());
+			}
+		});
+
+		dependingOnValues.addValueChangeListener(e -> {
+
+			jsonData.put("dependingOnValues", e.getValue().toLowerCase());
+		});
+
+		styles.addValueChangeListener(e -> {
+
+			jsonData.put("styles", e.getValue().toString());
+		});
+
+		constraints.addValueChangeListener(e -> {
+
+			if (e.getValue().toString().toLowerCase().equals("expression")) {
+
+				jsonData.put("constraints", "expression");
+			} else {
+
+				min.setVisible(true);
+				max.setVisible(true);
+			}
+		});
+
+		min.addValueChangeListener(e -> {
+
+			if (e.getValue() != null) {
+
+				jsonData.put("constraints", "min" + e.getValue().toString() + ", ");
+			}
+
+		});
+
+		max.addValueChangeListener(e -> {
+
+			if (e.getValue() != null) {
+
+				String minValue = jsonData.get("constraints");
+				if (minValue != null) {
+
+					jsonData.put("constraints", minValue + "max " + e.getValue().toString());
+				}
+			}
+
+		});
+
+		errorMessage.addValueChangeListener(e -> {
+
+			if (e.getValue() != null) {
+
+				jsonData.put("errormessage", e.getValue().toString());
+			}
+		});
+
+		generateJson.addClickListener(e -> {
+
+			generateJson();
+		});
+
+		clearTextArea.addClickListener(e -> {
+
+			jsonContainer.clear();
+		});
+
+	}
+
+	public void generateJson() {
+
+		if (jsonContainer.getValue() != null) {
+			appender = new StringBuilder(jsonContainer.getValue() + ",");
+		} else {
+
+			appender = new StringBuilder(jsonContainer.getValue());
+		}
+
+		appender.append(gson.toJson(jsonData));
+		String main = appender.toString();
+		jsonContainer.setValue(main);
+
+		formType.setValue(CampaignFormElementType.LABEL);
+		formId.clear();
+		caption.clear();
+		important.clear();
+		constraints.clear();
+		min.clear();
+		max.clear();
+		dependingOn.clear();
+		dependingOnValues.clear();
+		styles.clear();
+		errorMessage.clear();
 	}
 
 }
