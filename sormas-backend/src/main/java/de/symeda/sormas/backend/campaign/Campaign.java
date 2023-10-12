3156fb49c4c6bcf9851899d32ec67291c8600560
Yahaya Yusuf <yyahaya222@yahoo.com>
2023-10-12 10:49:42 +0100

    refactor: refactor #202

--- sormas-backend/src/main/java/de/symeda/sormas/backend/campaign/Campaign.java
+++ sormas-backend/src/main/java/de/symeda/sormas/backend/campaign/Campaign.java
@@ -46,6 +46,7 @@ public class Campaign extends CoreAdo {
 	public static final String CAMPAIGN_DISTRICT_TABLE_NAME = "campaign_district";
 	public static final String CAMPAIGN_COMMUNITY_TABLE_NAME = "campaign_community";
 	
+	
 	public static final String NAME = "name";
 	public static final String ROUND = "round";
 	public static final String DESCRIPTION = "description";
