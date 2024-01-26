a02c1b62f192ec7dc4f757f9c98b224d6a093451
Segun <salamioluwasegun.a@gmail.com>
2024-01-26 21:03:30 +0100

    - IMPLEMENTING LAZY LOAD/PAGINATION ON FORMDATA API

--- sormas-api/src/main/java/de/symeda/sormas/api/campaign/data/CampaignFormDataFacade.java
+++ sormas-api/src/main/java/de/symeda/sormas/api/campaign/data/CampaignFormDataFacade.java
@@ -39,19 +39,17 @@ import de.symeda.sormas.api.report.JsonDictionaryReportModelDto;
 import de.symeda.sormas.api.user.FormAccess;
 import de.symeda.sormas.api.utils.SortProperty;
 
-
-
 @Remote
 public interface CampaignFormDataFacade {
 
 	CampaignFormDataDto saveCampaignFormData(@Valid CampaignFormDataDto dto);
-	
+
 	CampaignFormDataDto saveCampaignFormDataMobile(@Valid CampaignFormDataDto dto);
 
 	List<CampaignFormDataDto> getByUuids(List<String> uuids);
 
 	CampaignFormDataDto getCampaignFormDataByUuid(String campaignFormDataUuid);
-	
+
 	List<CampaignFormDataIndexDto> getCampaignFormDataByCreatingUser(String creatingUser);
 
 	void deleteCampaignFormData(String campaignFormDataUuid);
@@ -62,72 +60,88 @@ public interface CampaignFormDataFacade {
 
 	CampaignFormDataReferenceDto getReferenceByUuid(String uuid);
 
-	List<CampaignFormDataIndexDto> getIndexList(CampaignFormDataCriteria criteria, Integer first, Integer max, List<SortProperty> sortProperties);
-  
-	List<CampaignFormDataIndexDto> getIndexListPashto(CampaignFormDataCriteria criteria, Integer first, Integer max, List<SortProperty> sortProperties);
+	List<CampaignFormDataIndexDto> getIndexList(CampaignFormDataCriteria criteria, Integer first, Integer max,
+			List<SortProperty> sortProperties);
 
-	List<CampaignFormDataIndexDto> getIndexListDari(CampaignFormDataCriteria criteria, Integer first, Integer max, List<SortProperty> sortProperties);
+	List<CampaignFormDataIndexDto> getIndexListPashto(CampaignFormDataCriteria criteria, Integer first, Integer max,
+			List<SortProperty> sortProperties);
 
-	List<CampaignFormDataIndexDto> getByCompletionAnalysis(CampaignFormDataCriteria criteria, Integer first, Integer max, List<SortProperty> sortProperties, FormAccess frm);
+	List<CampaignFormDataIndexDto> getIndexListDari(CampaignFormDataCriteria criteria, Integer first, Integer max,
+			List<SortProperty> sortProperties);
 
-	List<CampaignFormDataIndexDto> getByTimelinessAnalysis(CampaignFormDataCriteria criteria, Integer first, Integer max, List<SortProperty> sortProperties, FormAccess frm);
+	List<CampaignFormDataIndexDto> getByCompletionAnalysis(CampaignFormDataCriteria criteria, Integer first,
+			Integer max, List<SortProperty> sortProperties, FormAccess frm);
 
-	
-	String getByCompletionAnalysisCount(CampaignFormDataCriteria criteria, Integer first, Integer max, List<SortProperty> sortProperties, FormAccess frm);
-	
-	List<CampaignFormDataIndexDto> getByCompletionAnalysisAdmin(CampaignFormDataCriteria criteria, Integer first, Integer max, List<SortProperty> sortProperties, FormAccess frm);
-	
-	String getByCompletionAnalysisCountAdmin(CampaignFormDataCriteria criteria, Integer first, Integer max, List<SortProperty> sortProperties, FormAccess frm);
+	List<CampaignFormDataIndexDto> getByTimelinessAnalysis(CampaignFormDataCriteria criteria, Integer first,
+			Integer max, List<SortProperty> sortProperties, FormAccess frm);
+
+	String getByCompletionAnalysisCount(CampaignFormDataCriteria criteria, Integer first, Integer max,
+			List<SortProperty> sortProperties, FormAccess frm);
+
+	List<CampaignFormDataIndexDto> getByCompletionAnalysisAdmin(CampaignFormDataCriteria criteria, Integer first,
+			Integer max, List<SortProperty> sortProperties, FormAccess frm);
+
+	String getByCompletionAnalysisCountAdmin(CampaignFormDataCriteria criteria, Integer first, Integer max,
+			List<SortProperty> sortProperties, FormAccess frm);
 
 	CampaignFormDataDto getExistingData(CampaignFormDataCriteria criteria);
 
 	long count(CampaignFormDataCriteria criteria);
 
-	List<CampaignDiagramDataDto> getDiagramData(List<CampaignDiagramSeries> diagramSeries, CampaignDiagramCriteria campaignDiagramCriteria);
-	List<CampaignDiagramDataDto> getDiagramDataCard(List<CampaignDiagramSeries> diagramSeries, CampaignDiagramCriteria campaignDiagramCriteria);
-	List<CampaignDiagramDataDto> getDiagramDataByGroups(List<CampaignDiagramSeries> diagramSeries, CampaignDiagramCriteria campaignDiagramCriteria);
-	
-	List<CampaignDiagramDataDto> getDiagramDataFlow(List<CampaignDiagramSeries> diagramSeries, CampaignDiagramCriteria campaignDiagramCriteria);
-	List<CampaignDiagramDataDto> getDiagramDataCardFlow(List<CampaignDiagramSeries> diagramSeries, CampaignDiagramCriteria campaignDiagramCriteria);
-	List<CampaignDiagramDataDto> getDiagramDataByGroupsFlow(List<CampaignDiagramSeries> diagramSeries, CampaignDiagramCriteria campaignDiagramCriteria);
-
-	List<CampaignDiagramDataDto> getDiagramDataByAgeGroup(
-		CampaignDiagramSeries diagramSeriesTotal,
-		CampaignDiagramSeries diagramSeries,
-		CampaignDiagramCriteria campaignDiagramCriteria);
-	
-	List<CampaignDiagramDataDto> getDiagramDataByAgeGroupCard(
-			CampaignDiagramSeries diagramSeriesTotal,
-			CampaignDiagramSeries diagramSeries,
+	List<CampaignDiagramDataDto> getDiagramData(List<CampaignDiagramSeries> diagramSeries,
 			CampaignDiagramCriteria campaignDiagramCriteria);
+
+	List<CampaignDiagramDataDto> getDiagramDataCard(List<CampaignDiagramSeries> diagramSeries,
+			CampaignDiagramCriteria campaignDiagramCriteria);
+
+	List<CampaignDiagramDataDto> getDiagramDataByGroups(List<CampaignDiagramSeries> diagramSeries,
+			CampaignDiagramCriteria campaignDiagramCriteria);
+
+	List<CampaignDiagramDataDto> getDiagramDataFlow(List<CampaignDiagramSeries> diagramSeries,
+			CampaignDiagramCriteria campaignDiagramCriteria);
+
+	List<CampaignDiagramDataDto> getDiagramDataCardFlow(List<CampaignDiagramSeries> diagramSeries,
+			CampaignDiagramCriteria campaignDiagramCriteria);
+
+	List<CampaignDiagramDataDto> getDiagramDataByGroupsFlow(List<CampaignDiagramSeries> diagramSeries,
+			CampaignDiagramCriteria campaignDiagramCriteria);
+
+	List<CampaignDiagramDataDto> getDiagramDataByAgeGroup(CampaignDiagramSeries diagramSeriesTotal,
+			CampaignDiagramSeries diagramSeries, CampaignDiagramCriteria campaignDiagramCriteria);
+
+	List<CampaignDiagramDataDto> getDiagramDataByAgeGroupCard(CampaignDiagramSeries diagramSeriesTotal,
+			CampaignDiagramSeries diagramSeries, CampaignDiagramCriteria campaignDiagramCriteria);
 
 	List<String> getAllActiveUuids();
 
 	List<CampaignFormDataDto> getAllActiveAfter(Date date);
-	
+
 	List<CampaignFormDataDto> getAllActive();
-	
-	List<CampaignFormDataDto> getAllActiveData();
+
+	List<CampaignFormDataDto> getAllActiveData(Integer first, Integer Max);
+
 	List<CampaignFormDataDto> getAllActiveRef();
 
 	void overwriteCampaignFormData(CampaignFormDataDto existingData, CampaignFormDataDto newData);
-	
+
 	List<CampaignFormDataDto> getCampaignFormData(String campaignformuuid, String formuuid);
-	
+
 	List<CampaignAggregateDataDto> getCampaignFormDataAggregatetoCSV(String campaignformuuid);
-	
+
 	List<MapCampaignDataDto> getCampaignDataforMaps();
-	
-	String getByClusterDropDown(CommunityReferenceDto community, CampaignFormMetaDto campaignForm, CampaignDto campaign);
-	
+
+	String getByClusterDropDown(CommunityReferenceDto community, CampaignFormMetaDto campaignForm,
+			CampaignDto campaign);
+
 	void deleteCampaignData(List<String> uuids);
-	
+
 	void verifyCampaignData(List<String> uuids, boolean isUnVerifying);
-	
-	List<CampaignFormDataIndexDto> getByCompletionAnalysisNew(CampaignFormDataCriteria criteria, List<SortProperty> sortProperties, FormAccess frms);
+
+	List<CampaignFormDataIndexDto> getByCompletionAnalysisNew(CampaignFormDataCriteria criteria,
+			List<SortProperty> sortProperties, FormAccess frms);
 
 	List<JsonDictionaryReportModelDto> getByJsonFormDefinitonToCSV();
-	
+
 	String getByJsonFormDefinitonToCSVCount();
 
 	int prepareAllCompletionAnalysis();
@@ -144,10 +158,9 @@ public interface CampaignFormDataFacade {
 
 	String getByTimelinessAnalysisCount(CampaignFormDataCriteria criteria, Integer first, Integer max,
 			List<SortProperty> sortProperties, FormAccess frms);
-	
+
 	List<CampaignFormDataIndexDto> getCreatingUsersUserType(String username);
 
 	boolean getVerifiedStatus(String uuid);
-	
 
 }
